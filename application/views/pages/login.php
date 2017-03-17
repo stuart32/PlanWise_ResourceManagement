82926e83834dd72cc364a280711b0a1e3430b0a3
Mario Vaselimov Vasilev <mvv1@hw.ac.uk>
2017-03-17 16:20:25 +0000

    added email for creating an account :quit

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -25,7 +25,7 @@
           <div class="checkbox">
             <label><input type="checkbox"> Remember me</label>
           </div>
-          <button type="submit" class="btn btn-default">Submit</button>
+          <button type="button" class="btn-outline-primary">Submit</button>
         </form>
       </div>
     </div>
