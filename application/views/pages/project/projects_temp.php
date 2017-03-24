14f3d033276f89d66df4a60524d182947ed7c67e
Mario Vaselimov Vasilev <mvv1@hw.ac.uk>
2017-03-24 19:07:31 +0000

    styling and table add

--- application/views/pages/project/projects_temp.php
+++ application/views/pages/project/projects_temp.php
@@ -2,24 +2,34 @@
 $(document).ready(function() {
     $('#myTable').DataTable();
 } );
-
+ 
 </script>
 
-<table id="myTable" class="display table table-striped table-bordered" cellspacing="0" width="100%">
-	<thead>
-		<?php
-foreach($info as $project) {?>
-				<tr>
+<div id="myContainerr">
+<table id="myTable" class="display table table-striped table-bordered" cellspacing="0" width="100%" margin-left="15px">
+    <thead>
+                <tr>
                 <th>Title</th>
                 <th>Start Date</th>
                 <th>End Date</th>
                 <th>Budget</th>
             </tr>
-<?php
+ 
+     </thead>
+     <tbody>
+<?php   foreach($info as $project) {  ?>
+        <tr>
+            <td><?php echo $project->title ?></td>
+            <td><?php echo $project->startDate ?></td>
+            <td><?php echo $project->endDate ?></td>
+            <td><?php echo $project->budget ?></td>
+        </tr>
+ <?php
 }
 ?>
-	 </thead>
-	 <tfoot>
+    </tbody>
+ 
+     <tfoot>
             <tr>
                 <th>Title</th>
                 <th>Start Date</th>
@@ -27,15 +37,5 @@ foreach($info as $project) {?>
                 <th>Budget</th>
             </tr>
         </tfoot>
-        <tbody>
-			
- <tr>
-<td><?php echo $project->title ?></td>
-<td><?php echo $project->startDate ?></td>
-<td><?php echo $project->endDate ?></td>
-<td><?php echo $project->budget ?></td>
- </tr>
-    </tbody>
     </table>
-
-
+</div>
