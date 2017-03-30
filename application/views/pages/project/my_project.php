98a4f90e07b63fb87b34d91d6df72e93866fd586
Mario Vaselimov Vasilev <mvv1@hw.ac.uk>
2017-03-30 17:02:41 +0100

    commit

---
+++ application/views/pages/project/my_project.php
@@ -0,0 +1,60 @@
+<div class="container">
+	
+<script>
+$(document).ready(function() {
+    $('#myTable').DataTable();
+} );
+ 
+</script>
+
+<script>
+	$(function(){
+    $('.table tr[data-href]').each(function(){
+        $(this).css('cursor','pointer').hover(
+            function(){ 
+                $(this).addClass('active'); 
+            },  
+            function(){ 
+                $(this).removeClass('active'); 
+            }).click( function(){ 
+                document.location = $(this).attr('data-href'); 
+            }
+        );
+    });
+});
+</script>
+
+
+<table id="myTable" class="display table table-striped table-bordered" cellspacing="0" width="100%">
+    <thead>
+             <tr>
+                <th>Title</th>
+                <th>Start Date</th>
+                <th>End Date</th>
+                <th>Budget</th>
+            </tr>
+ 
+     </thead>
+     <tbody>
+<?php   foreach($info as $project) {  ?>
+        <tr>
+            <td><a href=" <?php echo base_url() ; echo "index.php/find_project/"; echo $project->projectID; ?>"</a><?php echo $project->title ?></td>
+            <td><?php echo $project->startDate ?></td>
+            <td><?php echo $project->endDate ?></td>
+            <td><?php echo $project->budget ?></td>
+        </tr>
+ <?php
+}
+?>
+    </tbody>
+ 
+     <tfoot>
+            <tr>
+                <th>Title</th>
+                <th>Start Date</th>
+                <th>End Date</th>
+                <th>Budget</th>
+            </tr>
+        </tfoot>
+    </table>
+</div>
